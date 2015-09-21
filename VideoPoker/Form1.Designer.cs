baeb42dd7733d73588f44df6e67710ff1e8f4001
Austin Ivicevic <austin.ivicevic@outlook.com>
2015-09-21 09:29:03 -0700

    Upload Project

---
+++ VideoPoker/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace VideoPoker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic5 = new System.Windows.Forms.PictureBox();
+            this.chk1 = new System.Windows.Forms.CheckBox();
+            this.chk2 = new System.Windows.Forms.CheckBox();
+            this.chk3 = new System.Windows.Forms.CheckBox();
+            this.chk4 = new System.Windows.Forms.CheckBox();
+            this.chk5 = new System.Windows.Forms.CheckBox();
+            this.btnDeal = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCashOut = new System.Windows.Forms.Button();
+            this.lblCredit = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tmr1 = new System.Windows.Forms.Timer(this.components);
+            this.tmr2 = new System.Windows.Forms.Timer(this.components);
+            this.tmr3 = new System.Windows.Forms.Timer(this.components);
+            this.tmr4 = new System.Windows.Forms.Timer(this.components);
+            this.tmr5 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic1.Location = new System.Drawing.Point(143, 123);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(114, 154);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic3.Location = new System.Drawing.Point(371, 123);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(114, 154);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic3.TabIndex = 1;
+            this.pic3.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic2.Location = new System.Drawing.Point(257, 123);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(114, 154);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 2;
+            this.pic2.TabStop = false;
+            // 
+            // pic4
+            // 
+            this.pic4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic4.Location = new System.Drawing.Point(485, 123);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(114, 154);
+            this.pic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic4.TabIndex = 3;
+            this.pic4.TabStop = false;
+            // 
+            // pic5
+            // 
+            this.pic5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic5.Location = new System.Drawing.Point(599, 123);
+            this.pic5.Name = "pic5";
+            this.pic5.Size = new System.Drawing.Size(114, 154);
+            this.pic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic5.TabIndex = 4;
+            this.pic5.TabStop = false;
+            // 
+            // chk1
+            // 
+            this.chk1.AutoSize = true;
+            this.chk1.Location = new System.Drawing.Point(174, 301);
+            this.chk1.Name = "chk1";
+            this.chk1.Size = new System.Drawing.Size(48, 17);
+            this.chk1.TabIndex = 5;
+            this.chk1.Text = "Hold";
+            this.chk1.UseVisualStyleBackColor = true;
+            // 
+            // chk2
+            // 
+            this.chk2.AutoSize = true;
+            this.chk2.Location = new System.Drawing.Point(287, 301);
+            this.chk2.Name = "chk2";
+            this.chk2.Size = new System.Drawing.Size(48, 17);
+            this.chk2.TabIndex = 6;
+            this.chk2.Text = "Hold";
+            this.chk2.UseVisualStyleBackColor = true;
+            // 
+            // chk3
+            // 
+            this.chk3.AutoSize = true;
+            this.chk3.Location = new System.Drawing.Point(403, 301);
+            this.chk3.Name = "chk3";
+            this.chk3.Size = new System.Drawing.Size(48, 17);
+            this.chk3.TabIndex = 7;
+            this.chk3.Text = "Hold";
+            this.chk3.UseVisualStyleBackColor = true;
+            // 
+            // chk4
+            // 
+            this.chk4.AutoSize = true;
+            this.chk4.Location = new System.Drawing.Point(521, 301);
+            this.chk4.Name = "chk4";
+            this.chk4.Size = new System.Drawing.Size(48, 17);
+            this.chk4.TabIndex = 8;
+            this.chk4.Text = "Hold";
+            this.chk4.UseVisualStyleBackColor = true;
+            this.chk4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // chk5
+            // 
+            this.chk5.AutoSize = true;
+            this.chk5.Location = new System.Drawing.Point(634, 301);
+            this.chk5.Name = "chk5";
+            this.chk5.Size = new System.Drawing.Size(48, 17);
+            this.chk5.TabIndex = 9;
+            this.chk5.Text = "Hold";
+            this.chk5.UseVisualStyleBackColor = true;
+            // 
+            // btnDeal
+            // 
+            this.btnDeal.Location = new System.Drawing.Point(65, 363);
+            this.btnDeal.Name = "btnDeal";
+            this.btnDeal.Size = new System.Drawing.Size(128, 41);
+            this.btnDeal.TabIndex = 10;
+            this.btnDeal.Text = "Deal";
+            this.btnDeal.UseVisualStyleBackColor = true;
+            this.btnDeal.Click += new System.EventHandler(this.btnDeal_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(663, 363);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(128, 41);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCashOut
+            // 
+            this.btnCashOut.Location = new System.Drawing.Point(364, 363);
+            this.btnCashOut.Name = "btnCashOut";
+            this.btnCashOut.Size = new System.Drawing.Size(128, 41);
+            this.btnCashOut.TabIndex = 12;
+            this.btnCashOut.Text = "Cash Out";
+            this.btnCashOut.UseVisualStyleBackColor = true;
+            // 
+            // lblCredit
+            // 
+            this.lblCredit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCredit.Location = new System.Drawing.Point(725, 40);
+            this.lblCredit.Name = "lblCredit";
+            this.lblCredit.Size = new System.Drawing.Size(114, 38);
+            this.lblCredit.TabIndex = 13;
+            this.lblCredit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Wide Latin", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(595, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 38);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Credit:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tmr1
+            // 
+            this.tmr1.Interval = 10;
+            this.tmr1.Tick += new System.EventHandler(this.tmr1_Tick);
+            // 
+            // tmr2
+            // 
+            this.tmr2.Interval = 10;
+            this.tmr2.Tick += new System.EventHandler(this.tmr2_Tick);
+            // 
+            // tmr3
+            // 
+            this.tmr3.Interval = 10;
+            this.tmr3.Tick += new System.EventHandler(this.tmr3_Tick);
+            // 
+            // tmr4
+            // 
+            this.tmr4.Interval = 10;
+            this.tmr4.Tick += new System.EventHandler(this.tmr4_Tick);
+            // 
+            // tmr5
+            // 
+            this.tmr5.Interval = 10;
+            this.tmr5.Tick += new System.EventHandler(this.tmr5_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 427);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCredit);
+            this.Controls.Add(this.btnCashOut);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDeal);
+            this.Controls.Add(this.chk5);
+            this.Controls.Add(this.chk4);
+            this.Controls.Add(this.chk3);
+            this.Controls.Add(this.chk2);
+            this.Controls.Add(this.chk1);
+            this.Controls.Add(this.pic5);
+            this.Controls.Add(this.pic4);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic5;
+        private System.Windows.Forms.CheckBox chk1;
+        private System.Windows.Forms.CheckBox chk2;
+        private System.Windows.Forms.CheckBox chk3;
+        private System.Windows.Forms.CheckBox chk4;
+        private System.Windows.Forms.CheckBox chk5;
+        private System.Windows.Forms.Button btnDeal;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCashOut;
+        private System.Windows.Forms.Label lblCredit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer tmr1;
+        private System.Windows.Forms.Timer tmr2;
+        private System.Windows.Forms.Timer tmr3;
+        private System.Windows.Forms.Timer tmr4;
+        private System.Windows.Forms.Timer tmr5;
+    }
+}
+
---
+++ VideoPoker/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace VideoPoker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Globals
+        string location = Directory.GetCurrentDirectory() + "/Deck/";
+        Image[] images = new Image[53];
+        int[,] cardVal = new int[53, 2];
+        Random rg = new Random();
+        static int animCount = 2;
+        static int cardCounter = 1;
+        double win = 100;
+        static int numOfClicks = 0;
+        enum SUITS { CLUBS = 1, DIAMONDS, HEARTS, SPADES };
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            //Accidentally put here
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+            int cardValue;
+            for(int i = 0; i < 53; ++i)
+            {
+                images[i] = Image.FromFile(location + i.ToString() + ".bmp");
+                if( i >= 1 && i <= 13) cardVal[i, 1] = (int) SUITS.CLUBS;
+                else if( i >= 14 && i <= 26) cardVal[i, 1] = (int) SUITS.DIAMONDS;
+                else if( i >= 27 && i <= 39) cardVal[i, 1] = (int) SUITS.HEARTS;
+                else if( i >= 40 && i <= 52) cardVal[i, 1] = (int) SUITS.SPADES;
+
+                cardValue = i % 13;
+                if(cardValue == 0) cardValue = 13;
+                else if (cardValue == 1) cardValue = 14;
+                cardVal[i, 0] = cardValue;
+            }
+
+            cardVal[0,0] = 0;
+            pic1.Image = pic2.Image = pic3.Image = pic4.Image = pic5.Image = images[0];
+            chk1.Focus();
+            ShuffleDeck();
+            lblCredit.Text = win.ToString("C");
+        }
+
+        private void ShuffleDeck()
+        {
+            Image tmp;
+            int r1, r2, temp;
+            for(int i = 1; i < 53; ++i)
+            {
+                r1 = rg.Next(1, 53);
+                r2 = rg.Next(1, 53);
+
+                //Shuffle
+                temp = cardVal[r1, 0];
+                cardVal[r1, 0] = cardVal[r2, 0];
+                cardVal[r2, 0] = temp;
+
+                //Shuffle Suits
+                temp = cardVal[r1, 1];
+                cardVal[r1, 1] = cardVal[r2, 1];
+                cardVal[r2, 1] = temp;
+
+                //Shuffle Images
+                tmp = images[r1];
+                images[r1] = images[r2];
+                images[r2] = tmp;
+
+            }
+        }
+
+        private void Animate(PictureBox p, int w)
+        {
+            p.Width = p.Width + w;
+        }
+
+        private void btnDeal_Click(object sender, EventArgs e)
+        {
+            if (numOfClicks < 2) tmr1.Enabled = true;
+            tmr2.Enabled = tmr3.Enabled = tmr4.Enabled = tmr5.Enabled = false;
+            numOfClicks++;
+            if(numOfClicks <= 2)
+            {
+                if(!chk1.Checked)
+                {
+                    animCount = 0;
+                    pic1.Image = images[cardCounter++];
+                    pic1.Width = animCount;
+                }
+
+                if (!chk2.Checked)
+                {
+                    animCount = 0;
+                    pic2.Image = images[cardCounter++];
+                    pic2.Width = animCount;
+                }
+
+                if (!chk3.Checked)
+                {
+                    animCount = 0;
+                    pic3.Image = images[cardCounter++];
+                    pic3.Width = animCount;
+                }
+
+                if (!chk4.Checked)
+                {
+                    animCount = 0;
+                    pic4.Image = images[cardCounter++];
+                    pic4.Width = animCount;
+                }
+
+                if (!chk5.Checked)
+                {
+                    animCount = 0;
+                    pic5.Image = images[cardCounter++];
+                    pic5.Width = animCount;
+                }
+                win -= .25;
+                lblCredit.Text = win.ToString();
+            }
+        }
+
+        private void tmr1_Tick(object sender, EventArgs e)
+        {
+            animCount = 2;
+            animCount++;
+            Animate(pic1, animCount);
+            if(pic1.Width >= 114)
+            {
+                tmr1.Enabled = false;
+                tmr2.Enabled = true;
+            }
+        }
+
+        private void tmr2_Tick(object sender, EventArgs e)
+        {
+            animCount = 2;
+            animCount++;
+            Animate(pic2, animCount);
+            if (pic2.Width >= 114)
+            {
+                tmr2.Enabled = false;
+                tmr3.Enabled = true;
+            }
+        }
+
+        private void tmr3_Tick(object sender, EventArgs e)
+        {
+            animCount = 2;
+            animCount++;
+            Animate(pic3, animCount);
+            if (pic3.Width >= 114)
+            {
+                tmr3.Enabled = false;
+                tmr4.Enabled = true;
+            }
+        }
+
+        private void tmr4_Tick(object sender, EventArgs e)
+        {
+            animCount = 2;
+            animCount++;
+            Animate(pic4, animCount);
+            if (pic4.Width >= 114)
+            {
+                tmr4.Enabled = false;
+                tmr5.Enabled = true;
+            }
+        }
+
+        private void tmr5_Tick(object sender, EventArgs e)
+        {
+            animCount = 2;
+            animCount++;
+            Animate(pic5, animCount);
+            if (pic5.Width >= 114)
+            {
+                tmr5.Enabled = false;
+            }
+        }
+    }
+}
+
